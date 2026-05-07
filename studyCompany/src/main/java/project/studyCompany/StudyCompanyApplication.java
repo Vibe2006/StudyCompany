f1955597ae538a23c0c5e2cdf638dcef9b90859e
EmpresaGlow <glow.etec@gmail.com>
2026-05-06 21:40:42 -0300

    first commit

---
+++ studyCompany/src/main/java/project/studyCompany/StudyCompanyApplication.java
@@ -0,0 +1,13 @@
+package project.studyCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCompanyApplication.class, args);
+	}
+
+}
---
+++ studyCompany/src/test/java/project/studyCompany/StudyCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package project.studyCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
